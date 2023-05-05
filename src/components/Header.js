3d32cdde9188f54f21daf23fa4cc2d1024ef1de3
ChristianFezard <christian.fezard@gmail.com>
2023-05-05 14:29:29 +0200

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -66,7 +66,7 @@ const Header = () => {
                     </div>
                 ) : (
                     <div className='accountLog'>
-                        <div className='profileName'>
+                        <div className='profileName'>  
                             <FontAwesomeIcon icon={faUserCircle} className="icon-sign"></FontAwesomeIcon>
                             {profileFirstName}
                         </div>
